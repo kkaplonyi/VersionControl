1ca06ec09e53354223d64e723b51dbf918001f54
Krisztián Kaplonyi <kkaplonyi@gmail.com>
2020-10-15 17:09:24 +0200

    ex 2-3

--- WebService/WebService/Form1.cs
+++ WebService/WebService/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WebService.MnbServiceReference;
 
 namespace WebService
 {
@@ -15,6 +16,20 @@ namespace WebService
         public Form1()
         {
             InitializeComponent();
+        }
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
