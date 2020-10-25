e40352c3656d3e89096c841648618438b376b63f
Krisztián Kaplonyi <kkaplonyi@gmail.com>
2020-10-25 13:58:20 +0100

    ex7

--- Simulation/Simulation/Form1.cs
+++ Simulation/Simulation/Form1.cs
@@ -17,6 +17,7 @@ namespace Simulation
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        Random rng = new Random(1234);
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +25,25 @@ namespace Simulation
             BirthProbabilities = GetBirthProbabilities(@"C:\Program Files (x86)\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Program Files (x86)\Temp\halál.csv");
             dataGridView1.DataSource = BirthProbabilities;
+
+            
+            for (int year = 2005; year <= 2024; year++)
+            {
+                
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SimStep(year, Population[i]);
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
         public List<Person> GetPopulation(string csvpath)
         {
@@ -84,6 +104,41 @@ namespace Simulation
             }
 
             return deathprobabilities;
+        }
+        private void SimStep(int year, Person person)
+        {
+            //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+            if (!person.IsAlive) return;
+
+            // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+            byte age = (byte)(year - person.BirthYear);
+
+            // Halál kezelése
+            // Halálozási valószínűség kikeresése
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+            // Meghal a személy?
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                //Szülési valószínűség kikeresése
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                //Születik gyermek?
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
     }
 }
