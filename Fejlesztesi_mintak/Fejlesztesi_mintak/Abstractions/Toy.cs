bee3c8243ea0c33fa94fcb70e43cdb2f1a059d7c
Krisztián Kaplonyi <kkaplonyi@gmail.com>
2020-11-15 11:31:46 +0100

    7.

---
+++ Fejlesztesi_mintak/Fejlesztesi_mintak/Abstractions/Toy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fejlesztesi_mintak.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        private void DrawImage(Graphics graphics)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected abstract void DrawImage(Graphics g);
+
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
+}
---
+++ Fejlesztesi_mintak/Fejlesztesi_mintak/Enities/Car.cs
@@ -0,0 +1,19 @@
+﻿using Fejlesztesi_mintak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fejlesztesi_mintak.Enities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
